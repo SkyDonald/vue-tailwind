5b29f658dbf09c4f736e43946e37dd72379a7fb7
SkyDonald <skydonald.dev@gmail.com>
2021-10-25 22:16:35 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center mt-3">
+    <router-link active-class="text-green-500" to="/">Home</router-link> |
+    <router-link active-class="text-green-500" to="/about">About</router-link>
+  </div>
+  <router-view />
+</template>
